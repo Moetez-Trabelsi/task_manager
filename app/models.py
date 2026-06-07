7c5879829b4bff676cb95e336525d729dbae8925
Trabelsi Med Moetez <mohamedmoeteztrabelsi@gmail.com>
2026-06-07 09:04:32 +0100

    fixing models.py

--- app/models.py
+++ app/models.py
@@ -20,8 +20,8 @@ class Task(Base):
     id= Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     description = Column(String)
-    is_completed = Collumn(Boolean, default=False)
-    owner_id = Collumn(Integer, ForeignKey("user.id"), nullable=False)
+    is_completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     created_at = Column(DateTime(timezone=True),server_default=func.now())
 
     owner = relationship("User", back_populates="tasks")
