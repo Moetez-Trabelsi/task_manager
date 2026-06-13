6d2249024a4ef570a060406856a555a83bdb6b9c
Trabelsi Med Moetez <mohamedmoeteztrabelsi@gmail.com>
2026-06-13 11:57:12 +0100

    fix all deprecation warnings

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
@@ -18,7 +18,7 @@ oauth_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def create_access_token(data:dict):
     to_encode=data.copy()
-    expire=datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
